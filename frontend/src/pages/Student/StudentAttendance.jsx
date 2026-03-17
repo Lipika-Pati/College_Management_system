ac64d668141e41983a6133d4f62be95ff576b666
Dibyasmita-Mohapatra <dibyasmita.m05@gmail.com>
2026-03-17 18:35:41 +0530

    Add attendance state and fetch API logic with useEffect

--- frontend/src/pages/Student/StudentAttendance.jsx
+++ frontend/src/pages/Student/StudentAttendance.jsx
@@ -1,2 +1,33 @@
 import { useEffect, useState } from "react";
 import api from "../../utils/api";
+
+const StudentAttendance = () => {
+
+    const token = localStorage.getItem("token");
+
+    const [attendance, setAttendance] = useState([]);
+
+    useEffect(() => {
+
+        const fetchAttendance = async () => {
+
+            try {
+
+                const res = await api.get(
+                    "/api/student/attendance",
+                    {
+                        headers: { Authorization: `Bearer ${token}` }
+                    }
+                );
+
+                setAttendance(res.data);
+
+            } catch (error) {
+                console.error(error);
+            }
+
+        };
+
+        fetchAttendance();
+
+    }, []);
