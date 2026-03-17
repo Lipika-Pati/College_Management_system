bfe562ad3740fb4c045ed40031a8164ab84bbc4e
Dibyasmita-Mohapatra <dibyasmita.m05@gmail.com>
2026-03-18 00:01:07 +0530

    chore: setup student layout with required imports and initial hooks

--- frontend/src/pages/Student/StudentLayout.jsx
+++ frontend/src/pages/Student/StudentLayout.jsx
@@ -1,1 +1,15 @@
+import { Link, Outlet, useLocation, useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+import api from "../../utils/api";
+import { Sun, Moon } from "lucide-react";
+
+const StudentLayout = () => {
+
+    const BASE_URL = api.defaults.baseURL;
+    const location = useLocation();
+    const navigate = useNavigate();
+    const token = localStorage.getItem("token");
+    
+
+        
 
